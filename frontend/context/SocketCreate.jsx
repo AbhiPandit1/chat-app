516f33062a4e2582d1964462f761c3a5ea411b05
Abhishek <224abhishekjha@gmail.com>
2024-06-12 22:52:54 +0100

    ddd

--- frontend/context/SocketCreate.jsx
+++ frontend/context/SocketCreate.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io('https://chat-app-production-gyne.onrender.com', {
+      const socket = io('http://localhost:4070', {
         query: {
           userId: authUser._id,
         },
