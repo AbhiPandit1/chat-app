e390cc74438ec47384bf6b752a3b64b0b0920c08
Abhishek <224abhishekjha@gmail.com>
2024-06-12 15:14:14 +0100

    Mongodb commit

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,8 @@ import cookieParser from 'cookie-parser';
 import authRoutes from './routes/authRoutes.js';
 import messageRoutes from './routes/messageRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import connectToMongoDb from './db/connectToMongoDb.js';
 
-import connectToMongoDB from './db/connectToMongoDB.js';
 import { app, server } from './socket/socket.js';
 
 dotenv.config();
@@ -31,6 +31,6 @@ app.get('*', (req, res) => {
 });
 
 server.listen(PORT, () => {
-  connectToMongoDB();
+  connectToMongoDb();
   console.log(`Server Running on port ${PORT}`);
 });
