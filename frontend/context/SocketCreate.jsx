3455bdf70017dea4d75ae55394154277e48c5e30
Abhishek <224abhishekjha@gmail.com>
2024-06-12 23:30:04 +0100

    Commit

--- frontend/context/SocketCreate.jsx
+++ frontend/context/SocketCreate.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io('http://localhost:4070', {
+      const socket = io('https://chat-app-1-3il4.onrender.com', {
         query: {
           userId: authUser._id,
         },
