40e069814be4990913651ee2a98aa0ab447d03aa
Abhishek <224abhishekjha@gmail.com>
2024-06-13 00:09:28 +0100

    cors added

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,26 @@
-import path from 'path';
 import express from 'express';
+import path from 'path';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors'; // Import CORS middleware
 
 import authRoutes from './routes/authRoutes.js';
 import messageRoutes from './routes/messageRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import connectToMongoDb from './db/connectToMongoDb.js';
-
 import { app, server } from './socket/socket.js';
 
 dotenv.config();
 
-// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
+// PORT should be assigned after calling dotenv.config()
 const PORT = process.env.PORT || 5000;
-
 const __dirname = path.resolve();
 
-app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(express.json()); // to parse incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
+
+// Enable CORS for all origins
+app.use(cors());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
